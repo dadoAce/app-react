690eb33e2ac81973819c7772b7b807f9f4c1debb
perlamaria06 <perlam.lopsan@gmail.com>
2022-06-13 20:07:38 -0500

    avance

--- screen/companyname/companyname.js
+++ screen/companyname/companyname.js
@@ -1,9 +1,8 @@
 import { ImageBackground, Text, View, SafeAreaView } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, Colors, ProgressBar } from 'react-native-paper';
 import { TextInput, Title, Headline } from 'react-native-paper';
 import { useState } from 'react';
 import * as React from 'react';
-import * as Progress from 'react-native-progress';
 import { Appbar } from 'react-native-paper';
 import { styles_basepantallas } from '../../styles/EstilosP';
 import { ScrollView } from 'react-native-web';
@@ -26,25 +25,26 @@ export default function getstarted ({ navigation }) {
                
             </Appbar.Header>
 
-            <View style={{  flex: 8, height: "100%", justifyContent: "flex-end"}}>
+            <View style={{  flex: "100%", height: "100%", justifyContent: "100%"}}>
 
-                  <View style={{flex: 2, height: "100%", justifyContent: "flex-end", paddingTop: "5%", 
-                  width:"100%", display:"flex"}}>
+                  <View style={{flex:3, height: "100%", justifyContent: "100%",
+                           padding: "5%",display: "100%"}}>
 
                    <Text
-                      style={{ color: '#000', marginBottom: 50, textAlign: "left", width: "100%", 
-                      width: 353, height:68, fontWeight:700, fontSize: 28, alignItems: "left" , 
-                      fontStyle: 'normal',fontWeight: 'bold'
+                      style={{ color: '#000', marginBottom:10 , width: "100%", 
+                      fontWeight:700, fontSize: 28, fontStyle: 'normal', width: 353, 
+                      height: 68, alignItems: "center", textAlign: "left"
                          }}>
+                          
 
                      Información basica de ti y tu negocio
 
                     </Text>
                   </View>
 
-                   <View style={[styles_basepantallas.margenLateral, { flex: 2}]}>
-            
-                     
+                   <View style={[styles_basepantallas.margenLateral, { flex: 12}]}>
+          
+
                     <TextInput
                      label="Nombre"
                      underlineColor='transparent'
@@ -52,8 +52,7 @@ export default function getstarted ({ navigation }) {
                      onChangeText={text7 => setText7(text7)}
                      style={{ marginTop: 5, height: 50, marginBottom: 15, backgroundColor: '#fff', 
                      borderBottomColor: '#ECECEC', borderRadius: 50,  margin: 5,
-                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8, elevation: 0,shadowOpacity:0, 
-                     shadowRadius:0, shadowOffset:0, shadowColor: "black" , textShadowRadius: 1 
+                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8,  shadowColor: "black"  
                      }}/>
                     
                     <TextInput
@@ -63,36 +62,39 @@ export default function getstarted ({ navigation }) {
                      onChangeText={text8 => setText8(text8)}
                      style={{ marginTop: 5, height: 50, marginBottom: 15, backgroundColor: '#fff', 
                      borderBottomColor: '#ECECEC', borderRadius: 50,  margin: 5,
-                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8, elevation: 0,shadowOpacity:0, 
-                     shadowRadius:0, shadowOffset:0, shadowColor: "black" , textShadowRadius: 1 }}
-                    />
+                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8,  shadowColor: "black"  
+                    }}/>
                     <TextInput
                      label="Compañia (Opcional)"
                      underlineColor='transparent'
                      value={text9}
                      onChangeText={text9 => setText9(text9)}
-                     style={{  marginTop: 5, height: 50, marginBottom: 15, backgroundColor: '#fff', 
-                     borderBottomColor: '#ECECEC', borderRadius: 50,  margin: 5,
-                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8, elevation: 0,shadowOpacity:0, 
-                     shadowRadius:0, shadowOffset:0, shadowColor: "black" , textShadowRadius: 1  }}
-                     />
+                     style={{  marginTop: 5, height: 50, marginBottom: '80%', backgroundColor: '#fff', 
+                     borderBottomColor: '#ECECEC', borderRadius: 50,  margin: 5, 
+                     borderWidth: 3, borderColor: "#ECECEC", borderRadius: 8, shadowColor: "black"  
+                     }}/>
 
                    </View>
+                   <View  >
+                      <View style={{ flex: 4, height: "100%", width: "100%" ,justifyContent: "flex-end", 
+                               paddingTop:"5%", paddingBottom:"5%", margenLateral:"5%" , color: '#ECECEC'}}>
+                      <View >
+                        <ProgressBar progress={0.25} color={Colors.yellow500}
+                                style= {{backgroundColor:'#ECECEC', borderRadius: '10',
+                                         margin:"5%"}}>
 
-
-
-
-                <View style={{ flex: 3, height: "100%", justifyContent: "flex-end", 
-                paddingTop:"5%", paddingBottom:"5%" }}>
+                        </ProgressBar>
+                      </View>
+               
                     <Button 
                         touchableOpacity
                         alignItems= 'Flex-end'
                         mode="contained"
                         onPress={() => navigation.navigate("Dashboard")}
-                         style={styles_basepantallas.boton_siguiente}>
+                        style={[styles_basepantallas.boton_siguiente, {margenLateral:"5%" , margin: "3%"}]}>
 
                      <Text style={{fontFamily: 'Monserrat', fontStyle: 'normal', width:"105",
-                                   height:"24",paddingTop:"5%", paddingBottom:"5%"  }}> 
+                                   height:"24", }}> 
                      </Text>
                       <Text
                         style={styles_basepantallas.btn_texnegro}>
@@ -102,8 +104,7 @@ export default function getstarted ({ navigation }) {
                      </Button>
                
                 </View>
-                
-        
+                </View>
          </View>
          </ScrollView>
        </View>
